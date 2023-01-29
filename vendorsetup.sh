fd0c9340e91d1c9f2bdae350dc24518834408601
Niranjan BR <niranjankannan2003@gmail.com>
2023-01-29 07:31:04 +0000

    sweet: vendorsetup.sh: Include Leica Camera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,6 @@
 rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
-rm -rf packages/resources/devicesettings
 
 # Hals
 git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/audio 
@@ -18,3 +17,6 @@ git clone https://github.com/Sm6150-Sweet/vendor_xiaomi.git -b 13.0 vendor/xiaom
 # Kernel
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
 git clone -b meraki --depth=1 https://github.com/Sm6150-Sweet/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+
+# Leica Camera
+git clone https://gitlab.com/mrfox2003/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
