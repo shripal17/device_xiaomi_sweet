5f463844a2ad1e208f39767a57b68dcec4e0435c
chuspita <kosecki99@gmail.com>
2023-03-22 10:59:21 +0100

    sweet: Changes Alphadroid

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,14 +11,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Voltage OS  Stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 2160
-PRODUCT_NAME := voltage_sweet
+# Inherit common Alphadroid  Stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Alphadroid Stuffs
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps
+WITH_GAPPS := true
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+
+# Blaze Maintainer
+ALPHA_MAINTAINER := franlop77
+ALPHA_BUILD_TYPE := Official
+
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-VOLTAGE_BUILD_TYPE := OFFICIAL
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
