45b90549a829c071e3c3d0ea88053207670c120c
JYR_RC <119703887+JYRRC@users.noreply.github.com>
2023-04-10 21:39:31 +0200

    Update colt_sweet.mk

--- colt_sweet.mk
+++ colt_sweet.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit common PixysOS  Stuff.
+# Inherit common ColtOS  Stuff.
 $(call inherit-product, vendor/colt/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
