802e693e06de098f8e5fae04a55741d448d1576a
Shripal Jain <shripal17@gmail.com>
2023-04-27 01:25:41 +0530

    sweet: Temporarily disable miuicam
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,7 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-13.0-c
 git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # Leica Camera
-git clone https://gitlab.com/mrfox2003/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+# git clone https://gitlab.com/mrfox2003/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
 
 # Kernel
 git clone -b 17 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
